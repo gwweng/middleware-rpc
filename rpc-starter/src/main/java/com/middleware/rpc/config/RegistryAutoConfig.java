adafc15f08608d424fad30d894c4bc722425db3f
gengweiweng <gengweiweng@gmail.com>
2021-08-09 15:48:54 +0800

    <更新>fix redis配置类加载不到的问题

--- rpc-starter/src/main/java/com/middleware/rpc/config/RegistryAutoConfig.java
+++ rpc-starter/src/main/java/com/middleware/rpc/config/RegistryAutoConfig.java
@@ -1,36 +1,44 @@
 package com.middleware.rpc.config;
 
+import com.middleware.rpc.domain.LocalServerInfo;
+import com.middleware.rpc.util.ExecutorUtils;
 import lombok.extern.slf4j.Slf4j;
+import com.middleware.rpc.network.server.RegistryServerSocket;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
+ * 开启RPC功能，自动连接注册中心
  * @author gengweiweng
  * @time 2021/8/5
  * @desc
  */
+@Slf4j
 @Configuration
 @EnableConfigurationProperties(RegistryProperties.class)
-@Slf4j
-public class RegistryAutoConfig implements BeanFactoryPostProcessor, ApplicationContextAware {
-
-
-
-    @Override
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
-        log.info("初始化启动注册中心服务端");
-        // TODO:异步启动注册中心服务端
-        
-
-    }
+public class RegistryAutoConfig implements ApplicationContextAware {
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println("测试");
+        log.info("初始化启动注册中心服务端");
+        // 异步启动注册中心服务端
+        ThreadPoolExecutor threadPool = ExecutorUtils.getThreadPool();
+
+        RegistryServerSocket registryServerSocket = new RegistryServerSocket(applicationContext);
+        threadPool.execute(registryServerSocket);
+        // 异步线程执行未完成
+        while (!registryServerSocket.isActiveSocketServer()){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+
+            }
+        }
+        log.info("初始化注册中心服务端完成，ip=>{}, port=>{}", LocalServerInfo.LOCAL_HOST, LocalServerInfo.LOCAL_PORT);
     }
 }
