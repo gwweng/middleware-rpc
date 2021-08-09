4ef04495889afd8c6b45f38760d857dfc44cae76
gengweiweng <gengweiweng@gmail.com>
2021-08-09 15:47:29 +0800

    <更新>完成服务提供者的服务注册、暴露

--- rpc-provider/src/main/java/com/middleware/provider/RpcProviderApplication.java
+++ rpc-provider/src/main/java/com/middleware/provider/RpcProviderApplication.java
@@ -1,12 +1,13 @@
 package com.middleware.provider;
 
+import com.middleware.rpc.annotation.EnableRPC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.ImportResource;
 
-@ComponentScan("com.middleware.*")
+@EnableRPC
 @SpringBootApplication
+@ImportResource(locations = {"classpath:spring-config.xml"})
 public class RpcProviderApplication {
 
     public static void main(String[] args) {
